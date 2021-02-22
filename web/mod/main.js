deeea2de0202431e1def50321e4f5b0ce0a70ca8
Markus <markus@blaumond.net>
2021-02-22 16:58:48 +0100

    add routing and page inlining

--- web/mod/main.js
+++ web/mod/main.js
@@ -40,43 +40,61 @@ if (sessionStorage.href) {
                     query,
                 };
             }
-            return null;
 
-            function split_at(str, del) {
-                let i = (str.indexOf(del) + str.length + 1) % (str.length + 1);
-                return [str.substring(0, i), str.substring(i + 1)];
-            }
+            return null;
         },
         (args) => {
             document.body.innerHTML = "";
-            let main = document.body.appendChild(document.createElement("main"));
 
-            leaf(main)
-                .h1(JSON.stringify(args, null, 4), (node) => {
-                    node.style.whiteSpace = "pre";
-                })
-                .p(
-                    leaf()
-                        .t("Welcome to ")
-                        .i("Corona Lage")
-                        .t(". This is your daily update of Covid-19 incidence values across Germany. Go ahead and visit a ")
-                        .a(
-                            "random page",
-                            "{HOME}/" +
-                                lorem(rand_between(0, 4))
-                                    .toLowerCase()
-                                    .replace(/[^a-z ]+/g, "")
-                                    .replace(/ /g, "/")
-                        )
-                        .t(".")
-                )
-                .ul(new Array(rand_between(3, 12)).fill(null).map(() => lorem(rand_between(5, 20))))
-                .p(lorem(rand_between(30, 70)));
+            let main = document.body.appendChild(document.createElement("main"));
+            leaf(main).append(router(args));
+            if (main.childNodes.length === 0) {
+                leaf(main).h1("Status 404").p("There is nothing here, are you lost?").p(leaf().a("Go home", "{HOME}/").t("."));
+            }
 
             let footer = document.body.appendChild(document.createElement("footer"));
             footer.textContent = "markus";
         }
     );
+}
+
+function router(args) {
+    if (args.path.length === 0) {
+        return leaf()
+            .h1("home")
+            .append(router({path: ["foo"]}));
+    } else if (args.path[0] !== "foo") {
+        return leaf()
+            .h1(JSON.stringify(args, null, 4), (node) => {
+                node.style.whiteSpace = "pre";
+            })
+            .p(
+                leaf()
+                    .t("Welcome to ")
+                    .i("Corona Lage")
+                    .t(". This is your daily update of Covid-19 incidence values across Germany. Go ahead and visit a ")
+                    .a(
+                        "random page",
+                        "{HOME}/" +
+                            lorem(rand_between(0, 4))
+                                .toLowerCase()
+                                .replace(/[^a-z ]+/g, "")
+                                .replace(/ /g, "/")
+                    )
+                    .t(".")
+            )
+            .append(router({path: [], query: {}}))
+            .ul(new Array(rand_between(3, 12)).fill(null).map(() => lorem(rand_between(5, 20))))
+            .p(lorem(rand_between(30, 70)));
+    }
+
+    console.error("unknown resource: %o", {path: "{HOME}/" + args.path.join("/")});
+    return leaf();
+}
+
+function split_at(str, del) {
+    let i = (str.indexOf(del) + str.length + 1) % (str.length + 1);
+    return [str.substring(0, i), str.substring(i + 1)];
 }
 
 function rand_between(min, max) {
