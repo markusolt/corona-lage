4e71b9c7329558e89408093b2e2dd114d20964ad
Markus <markus@blaumond.net>
2021-02-27 09:29:32 +0100

    provide a 200.html for servers that support it

--- web/build.ps1
+++ web/build.ps1
@@ -27,8 +27,8 @@ $js_hash = (get-filehash "$target_dir/index.js").hash.substring(0, 5);
 ((get-content -raw "./index.css") -replace "\s+", " ").replace("{HOME}", $home_url) | set-content "$target_dir/index.css";
 $css_hash = (get-filehash "$target_dir/index.css").hash.substring(0, 5);
 
-(get-content -raw "./index.html").replace("{HOME}", $home_url).replace("{JS_HASH}", $js_hash).replace("{CSS_HASH}", $css_hash) | set-content -nonewline "$target_dir/index.html";
-$html_hash = (get-filehash "$target_dir/index.html").hash.substring(0, 5);
+(get-content -raw "./200.html").replace("{HOME}", $home_url).replace("{JS_HASH}", $js_hash).replace("{CSS_HASH}", $css_hash) | set-content -nonewline "$target_dir/200.html";
+$html_hash = (get-filehash "$target_dir/200.html").hash.substring(0, 5);
 
 (get-content -raw "./manifest.webmanifest").replace("{HOME}", $home_url) | set-content -nonewline "$target_dir/manifest.webmanifest";
 (get-content -raw "./404.html").replace("{HOME}", $home_url).replace("{HTML_HASH}", $html_hash) | set-content -nonewline "$target_dir/404.html";
